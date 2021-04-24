ea78a21c3681a1a4e00631a4360bba162bbec5f4
Nazrul Chowdhury <nazrulchowdhury404@gmail.com>
2021-04-24 12:57:32 +0100

    function symbles changed

--- cypress/integration/UI/login.spec.js
+++ cypress/integration/UI/login.spec.js
@@ -1,18 +1,18 @@
 import * as user from '../../fixtures/userInfo'
 
-describe('login test with diffferent credentials',function(){
+describe('login test with diffferent credentials',()=>{
     beforeEach(()=>{
         cy.visit('/login')
     })
-    it('sign in attempt fails with wrong email',function(){      
+    it('sign in attempt fails with wrong email',()=>{      
         cy.signIn(user.wrongEmail, user.password)
         cy.contains('email or password is invalid')
     })
-    it('sign in attempt fails with wrong password',function(){       
+    it('sign in attempt fails with wrong password',()=>{       
         cy.signIn(user.email, user.wrongPassword)
         cy.contains('email or password is invalid')
     })
-    it('sign in attempt successful with correct credentials',function(){
+    it('sign in attempt successful with correct credentials',()=>{
         cy.signIn(user.email, user.password)
         cy.wait(1000)
         cy.menu(user.name).should('exist')
